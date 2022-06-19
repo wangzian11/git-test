ffca2151317295448652083ee679398a287406a9
wangzian <a306989877@163.com>
2022-06-19 11:41:52 +0800

    hot-fix commit2

--- src/main/java/com/bjpowernode/git/GitTest.java
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello world!");
         System.out.println("hello git2");
         System.out.println("hello git4");
+        System.out.println("hello hot-fix2");
     }
 }
